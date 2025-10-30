273323441de24ea2f51c3b14d0d1f3da097bdc9b
unknown <heldheldheldheldheldheldheld@gmail.com>
2025-10-30 15:06:03 +0100

    Präsenzaufgabe Nr.2: Ausgaben angepasst

--- src/p2/P2_main.java
+++ src/p2/P2_main.java
@@ -3,11 +3,12 @@ package p2;
 public class P2_main {
 	
 	public static void main(String[] args) {
-		int i = 5;
+		int i = 50;
 		boolean b = false;
 		if(i > 10) {
 			b = true;
 		}
-		System.out.println(b);
+		System.out.println("Wert von i: " + i);
+		System.out.println("Wert von b: " + b);
 	}
 }
--- src/p3/P3_main.java
+++ src/p3/P3_main.java
@@ -1,5 +1,13 @@
 package p3;
 
 public class P3_main {
-
+	
+	public static void main(String[] args) {
+		int i = 35;
+		int j = 123;
+		int k = 888;
+		if(i < j) System.out.println("A");
+		if(i < k) System.out.println("B");
+		if(j < k) System.out.println("C");
+	}
 }
